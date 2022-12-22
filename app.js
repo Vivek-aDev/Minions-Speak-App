b3f06d2ccc2da91f218786ab443b2b219233cff3
VIVEKANANDA <vivekananda.work2022@gmail.com>
2022-12-23 01:53:46 +0530

    wrote clean code

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-var txtInput = document.querySelector("#txt-input");
+// Linking from html world to javascript world 
 var btnTranslate = document.querySelector("#btn-translate");
-var outputDiv = document.querySelector("#output");
+var txtInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output")
 
+// server url stored in placeholder
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-
-function getTranslationURL(input) {
-    return serverURL + "?" + "text" + input;
-}
-
-
-function errorHandler(error) {
-    console.log("error occured", error);
-    alert("Something went wrong with server!, Please try after some time.");
-}
-
+// adding event listener to button
+btnTranslate.addEventListener("click", clickHandler)
 
 function clickHandler() {
     var inputText = txtInput.value;
-
+    outputDiv.innerText = "Translation in progress..." // message shown to client until we get translated value
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
-            var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText;
+            var translatedText = json.contents.translated
+            outputDiv.innerText = translatedText
         })
-    .catch(errorHandler)
+        // error handling
+        .catch(errorHandler) // calling a function
+
 };
 
-btnTranslate.addEventListener("click", clickHandler);
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+    //? and something= are query parameters
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something is wrong with server! try again later")
+}   
